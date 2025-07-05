7274c98f62a566adacf7b2cc76863792b9840d87
kirkgriffin <krkgrffntn@gmail.com>
2025-07-05 23:15:47 +0800

    refactor(users.staticdata): changed the static data for users to match database table

--- staticDatas/dummies/users.staticData.php
+++ staticDatas/dummies/users.staticData.php
@@ -1,9 +1,50 @@
 <?php
 
-return[
-    ['username' => 'john.smith',
-    'first_name' => 'John',
-    'last_name' => 'Smith',
-    'password' => 'p@ssW0rd1234',
-    'role' => 'designer'],
+
+return [
+  [
+    'fullname' => 'John Doe',
+    'password' => 'Admin@123', 
+    'contact_num' => '09171234567',
+    'username' => 'kirkgt',
+    'role' => 'admin',
+    'passport_img' => file_get_contents(IMAGES_PATH . '/kirk.jpg'),
+    'flight_id' => null, 
+  ],
+  [
+    'fullname' => 'Tony Smith',
+    'password' => 'Tonysmith@123',
+    'contact_num' => '09180001111',
+    'username' => 'tony',
+    'role' => 'client',
+    'passport_img' => file_get_contents(IMAGES_PATH . '/tony.jpg'),
+    'flight_id' => null,
+  ],
+  [
+    'fullname' => 'Peter Gayle',
+    'password' => 'Petergayle@123',
+    'contact_num' => '09223334444',
+    'username' => 'peter',
+    'role' => 'client',
+    'passport_img' => file_get_contents(IMAGES_PATH . '/peter.jpg'),
+    'flight_id' => null,
+  ],
+  [
+    'fullname' => 'Bruce Clarks',
+    'password' => 'Bruceclarks@123',
+    'contact_num' => '09998887777',
+    'username' => 'bruce',
+    'role' => 'client',
+    'passport_img' => file_get_contents(IMAGES_PATH . '/bruce.jpg'),
+    'flight_id' => null,
+  ],
+  [
+    'fullname' => 'Clark Johnson',
+    'password' => 'Clarkjohnson@123',
+    'contact_num' => '09179998888',
+    'username' => 'clark',
+    'role' => 'client',
+    'passport_img' => file_get_contents(IMAGES_PATH . '/clark.jpg'),
+    'flight_id' => null,
+  ],
 ];
