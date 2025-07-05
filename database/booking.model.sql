bbe7281d5315cfd9d5b075171d02ccaa7825ab20
tianjose09 <202310267@fit.edu.ph>
2025-07-05 21:55:51 +0800

    refactor(databse): deleted the unnecessary sql.

---
+++ database/booking.model.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS public.bookings (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id INT REFERENCES users(id),
+  flight_id INT REFERENCES flights(id),
+  booking_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  travel_date DATE,
+  seat_number VARCHAR(10),
+  ticket_code VARCHAR(20) UNIQUE,
+  status VARCHAR(20) DEFAULT 'booked', -- values: booked, completed, cancelled
+  feedback TEXT
+);
