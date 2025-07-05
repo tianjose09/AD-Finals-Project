76aea0ea28423b539d96cb89506f58ee465d6586
kirkgriffin <krkgrffntn@gmail.com>
2025-07-05 22:11:29 +0800

    refactor(users.model.sql): renamed filename

---
+++ database/users.model.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS public."users" (
+    id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    fullname VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(225) NOT NULL,
+    contact_num VARCHAR(11),
+    username VARCHAR(225) NOT NULL UNIQUE,
+    role VARCHAR(20) NOT NULL CHECK (role IN ('admin', 'client')),
+    passport_img BYTEA, --for passport images
+    flight_id UUID REFERENCES flights(id)
+);
