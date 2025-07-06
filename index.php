8ef631f759c9bff938375c82de1912af1be12583
kirkgriffin <krkgrffntn@gmail.com>
2025-07-06 15:25:06 +0800

    feat(index.php): added require_once for index.html landing page

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
-require_once HANDLERS_PATH . '/mongodbChecker.handler.php';
-require_once HANDLERS_PATH . '/postgreChecker.handler.php';
+#require_once HANDLERS_PATH . '/mongodbChecker.handler.php';
+#require_once HANDLERS_PATH . '/postgreChecker.handler.php';
 
 ?>
 <html>
     <body>
     <?php 
-    include_once HANDLERS_PATH . "/mongodbChecker.handler.php";
-    include_once HANDLERS_PATH . "/postgreChecker.handler.php";
+    #include_once HANDLERS_PATH . "/mongodbChecker.handler.php";
+    #include_once HANDLERS_PATH . "/postgreChecker.handler.php";
+    require_once BASE_PATH . "/index.html";
     ?>
     </body>
 </html>
