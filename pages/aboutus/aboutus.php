1b2d85d1c177f86fbb1f17498b35f6c62d2719aa
Sobrielle <sgscalalay@gmail.com>
2025-07-10 18:19:02 +0800

    fit(about us): Completed the front-end

--- pages/aboutus/aboutus.php
+++ pages/aboutus/aboutus.php
@@ -3,15 +3,136 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Passenger Details</title>
-    <link rel="stylesheet" href="styles.css">
+    <title>About Us</title>
 
+    <link rel="stylesheet" href="assets/navbar/css/navbar.css">
+    <link rel="stylesheet" href="assets/css/bg.css">
+    <link rel="stylesheet" href="assets/footer/css/footer.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="icon" type="image/x-icon" href="assets/img/TED_LOGOwoBG.png">
 
 </head>
-
 <body>
-    <?php require_once TEMPLATES_PATH . '/navbar.template.html'; ?>
 
+    <!-- NAVBAR -->
+    <nav class="navbar">
+        <!-- Logo -->
+        <div class="logo">
+            <img src="assets/navbar/img/TED_LOGO_White.png" alt="Teddiursa Airlines Logo" draggable="false">
+        </div>
+
+        <div class="nav-items-container">
+            <div class="nav-item">
+                <div class="nav-icon">
+                    <img src="assets/navbar/img/Profile.png" alt="Account" draggable="false">
+                </div>
+                <span class="nav-text">Account</span>
+            </div>
+
+            <div class="nav-item">
+                <div class="nav-icon">
+                    <img src="assets/navbar/img/Explore.png" alt="Explore" draggable="false">
+                </div>
+                <span class="nav-text">Explore</span>
+            </div>
+
+            <div class="nav-item">
+                <div class="nav-icon">
+                    <img src="assets/navbar/img/Book.png" alt="Book a trip" draggable="false">
+                </div>
+                <span class="nav-text">Book a trip</span>
+            </div>
+
+            <div class="nav-item">
+                <div class="nav-icon">
+                    <img src="assets/navbar/img/AboutUs.png" alt="About Us" draggable="false">
+                </div>
+                <span class="nav-text">About Us</span>
+            </div>
+        </div>
+    </nav>
+
+
+    <!--ABOUT US-->
+    <div class="cont-aboutUs">
+        <img src="assets/img/Banner_Aboutus.png" alt="About Us Banner" draggable="false">
+
+        <div id="au-text">
+            <p>At Teddiursa Airlines, we're not just redefining travel -- <br>
+            we're reimagining the future. Designed for the bold and the curious <br>
+            our mission is to make space accessible, safe, and unforgettable.<br>
+            Whether you're heading to low Earth orbit or venturing farther <br>
+            we combine advanced aerospace technology with exceptional <br>
+            service to deliver more than just a flight.<br><br></p>
+            <h2>It's not just a flight. It's a leap into the universe.</h2>
+        </div>
+
+        <div class="dvdr"><img src="assets/img/divider.png" alt="divider" draggable="false"></div>
+
+        <div class="team">
+        <h1>Meet the TEAM</h1>
     
+        <div class="team-row">
+            <div id="calalay">
+                <img src="assets/img/Calalay.png" alt="Calalay Profile" draggable="false">
+                <h3>SOPHIA CALALAY</h3>
+                <p>Front-End</p>
+            </div>
 
+            <div id="emit">
+                <img src="assets/img/Emit.png" alt="Emit Profile" draggable="false">
+                <h3>YUKI EMIT</h3>
+                <p>Back-End</p>
+            </div>
+
+            <div id="guinto">
+                <img src="assets/img/Guinto.png" alt="Guinto Profile" draggable="false">
+                <h3>KHRISTIAN GUINTO</h3>
+                <p>Database/Leader</p>
+            </div>
+        </div>
+    
+        <div class="team-row">
+            <div id="sadicon">
+                <img src="assets/img/Sadicon.png" alt="Sadicon Profile" draggable="false">
+                <h3>ALTHEA SADICON</h3>
+                <p>Front-End</p>
+            </div>
+
+            <div id="tan">
+                <img src="assets/img/Tan.png" alt="Tan Profile" draggable="false">
+                <h3>KIRK TAN</h3>
+                <p>Database/Back-End</p>
+            </div>
+        </div>
+        </div>
+
+    </div>
+
+    <!--FOOTER-->
+    <footer>
+        <div id="foot-logo">
+            <img src="assets/footer/img/TED_LOGOwBG.png" alt="Teddiursa Airlines Logo" draggable="false">
+        </div>
+
+        <div class="foot-text">
+            <div id="foot-tagline">
+                Not Just a Flight. A Leap Into the Universe.
+            </div>
+            <div id="foot-contInfo">
+                <h3>Contact Information:</h3>
+                <p><i>teddairlines@gmail.com  |  0912-343-5352</i></p>
+            </div>
+        </div>
+    </footer>
+    <!--BACKGROUND-->
+    <div class="stars" id="stars"></div>
+    <div class="blob blob-1"></div>
+    <div class="blob blob-2"></div>
+    <div class="blob blob-3"></div>
+
+    <script src="assets/js/bg.js"></script>
+    <script src="assets/js/script.js"></script>
 </body>
+
+</html>
