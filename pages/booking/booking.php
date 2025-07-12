b227efeab7066428628496913e352b8b5de2172b
AltheaZuleika <sadiconaltheazuleika@gmail.com>
2025-07-12 17:23:31 +0800

    fix(booking): change path directory reference

--- pages/booking/booking.php
+++ pages/booking/booking.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Passenger Details</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="/pages/booking/assets/css/styles.css">
 </head>
 <body>
     <div class="stars" id="stars"></div>
@@ -233,6 +233,6 @@
         </div>
     </div>
 
-    <script src="script.js"></script>
+    <script src="/pages/booking/assets/js/script.js"></script>
 </body>
 </html>
