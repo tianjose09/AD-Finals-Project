da7f804105d7b05abb6d97a7884cf75a69ea3a0d
yukiemit <202310242@fit.edu.ph>
2025-07-13 12:06:39 +0800

    refactor (index) removed comments

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
     require_once LANDING_PATH . '/main.php';
-//require_once HANDLERS_PATH . '/postgreChecker.handler.php';
 
 ?>
 <html>
     <body>
     <?php 
     include_once LANDING_PATH . '/main.php';
-    //include_once HANDLERS_PATH . "/postgreChecker.handler.php";
     ?>
     </body>
 </html>
