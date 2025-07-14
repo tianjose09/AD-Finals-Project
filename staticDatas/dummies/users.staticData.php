c4faeddc18aa309ae2fc21475340aa7f56c2f863
kirkgriffin <krkgrffntn@gmail.com>
2025-07-14 15:09:31 +0800

    refactor(users.staticdata): removed loadImageOrFail function and changed static data structure to handle images table referencing

--- staticDatas/dummies/users.staticData.php
+++ staticDatas/dummies/users.staticData.php
@@ -1,59 +1,54 @@
 <?php
 
-function loadImageOrFail(string $filename): string {
-  $path = IMAGES_PATH . '/' . $filename;
-
-  if (!file_exists($path)) {
-    throw new RuntimeException("❌ Image file not found: $path");
-  }
-
-  return file_get_contents($path);
-}
-
 return [
   [
+    'id' => '11111111',
     'fullname' => 'John Doe',
     'password' => 'Admin@123',
     'contact_num' => '09171234567',
     'username' => 'kirkgt',
     'role' => 'admin',
-    'passport_img' => loadImageOrFail('kirk.jpg'),
+    'passport_img_id' => '12222222',
     'flight_id' => null,
   ],
   [
+    'id' => '22222222',
     'fullname' => 'Tony Smith',
     'password' => 'Tonysmith@123',
     'contact_num' => '09180001111',
     'username' => 'tony',
     'role' => 'client',
-    'passport_img' => loadImageOrFail('tony.jpg'),
+    'passport_img_id' => '13333333',
     'flight_id' => null,
   ],
   [
+    'id' => '33333333',
     'fullname' => 'Peter Gayle',
     'password' => 'Petergayle@123',
     'contact_num' => '09223334444',
     'username' => 'peter',
     'role' => 'client',
-    'passport_img' => loadImageOrFail('peter.jpg'),
+    'passport_img_id' => '14444444',
     'flight_id' => null,
   ],
   [
+    'id' => '44444444',
     'fullname' => 'Bruce Clarks',
     'password' => 'Bruceclarks@123',
     'contact_num' => '09998887777',
     'username' => 'bruce',
     'role' => 'client',
-    'passport_img' => loadImageOrFail('bruce.jpg'),
+    'passport_img_id' => '15555555',
     'flight_id' => null,
   ],
   [
+    'id' => '55555555',
     'fullname' => 'Clark Johnson',
     'password' => 'Clarkjohnson@123',
     'contact_num' => '09179998888',
     'username' => 'clark',
     'role' => 'client',
-    'passport_img' => loadImageOrFail('clark.jpg'),
+    'passport_img_id' => '16666666',
     'flight_id' => null,
   ],
 ];
