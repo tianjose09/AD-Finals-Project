5abfc7c6e5499a77c4c30a68ea1ac8619a950e11
kirkgriffin <krkgrffntn@gmail.com>
2025-07-14 16:36:55 +0800

    refactor(images.staticdata): removed id and user id

--- staticDatas/dummies/images.staticData.php
+++ staticDatas/dummies/images.staticData.php
@@ -2,40 +2,30 @@
 
 return [
   [
-    'id' => '12222222',
-    'user_id' => '11111111',
     'filename' => 'kirk.jpg',
     'filepath' => '/assets/img/kirk.jpg',
     'mimetype' => 'image/jpeg',
     'size_bytes' => filesize(IMAGES_PATH . '/kirk.jpg'),
   ],
   [
-    'id' => '13333333',
-    'user_id' => '22222222',
     'filename' => 'tony.jpg',
     'filepath' => '/assets/img/tony.jpg',
     'mimetype' => 'image/jpeg',
     'size_bytes' => filesize(IMAGES_PATH . '/tony.jpg'),
   ],
   [
-    'id' => '14444444',
-    'user_id' => '33333333',
     'filename' => 'peter.jpg',
     'filepath' => '/assets/img/peter.jpg',
     'mimetype' => 'image/jpeg',
     'size_bytes' => filesize(IMAGES_PATH . '/peter.jpg'),
   ],
   [
-    'id' => '15555555',
-    'user_id' => '44444444',
     'filename' => 'bruce.jpg',
     'filepath' => '/assets/img/bruce.jpg',
     'mimetype' => 'image/jpeg',
     'size_bytes' => filesize(IMAGES_PATH . '/bruce.jpg'),
   ],
   [
-    'id' => '16666666',
-    'user_id' => '55555555',
     'filename' => 'clark.jpg',
     'filepath' => '/assets/img/clark.jpg',
     'mimetype' => 'image/jpeg',
