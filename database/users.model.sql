02cc63d48e28caf26a3de5719309de3986f3e83a
kirkgriffin <krkgrffntn@gmail.com>
2025-07-14 16:44:18 +0800

    fix(users.sql): fixed spelling for passport_image

--- database/users.model.sql
+++ database/users.model.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS public."users" (
     contact_num VARCHAR(11),
     username VARCHAR(225) NOT NULL UNIQUE,
     role VARCHAR(20) NOT NULL CHECK (role IN ('admin', 'client')),
-    passport_imgage_id uuid REFERENCES public."images"(id),
+    passport_image_id uuid REFERENCES public."images"(id),
     flight_id UUID REFERENCES flights(id)
 );
