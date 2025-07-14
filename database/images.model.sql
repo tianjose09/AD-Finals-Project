dc19c1c11a3715a15e0d2f69b79dd9e1d313bdac
kirkgriffin <krkgrffntn@gmail.com>
2025-07-14 16:37:31 +0800

    refactor(images.model.sql): removed user id reference on users table to avoid loop referencing

--- database/images.model.sql
+++ database/images.model.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS public. "images"(
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id uuid NOT NULL REFERENCES public."users"(id) ON DELETE CASCADE,
     filename varchar(255) NOT NULL,
     filepath varchar(255) NOT NULL,
     mimetype varchar(50) NOT NULL,
