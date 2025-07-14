475a0919f97bc1b8d07e3848e640c34d954d33b6
tianjose09 <202310267@fit.edu.ph>
2025-07-14 23:50:35 +0800

    refactor(Design): Fixed the pathing of the css for tha main.php.

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="/pages/login/css/style.css">
+    <link rel="stylesheet" href="./pages/login/css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="/assets/img/TED_LOGOwoBG.png">
 </head>
