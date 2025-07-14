0be44817fea81a8c8660080239b58c98affd06ba
kirkgriffin <krkgrffntn@gmail.com>
2025-07-14 14:27:05 +0800

    refactor(images.sql): changed spacing

--- database/images.model.sql
+++ database/images.model.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS public. "images"(
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id uuid NOT NULL REFERENCES public. "users" (id) ON DELETE CASCADE,
+    user_id uuid NOT NULL REFERENCES public."users"(id) ON DELETE CASCADE,
     filename varchar(255) NOT NULL,
     filepath varchar(255) NOT NULL,
     mimetype varchar(50) NOT NULL,
