4fd19779ef47c8d55ac4202da44a94adc05e29fd
tianjose09 <202310267@fit.edu.ph>
2025-07-16 02:10:59 +0800

    refactor(flights): Fixed the position of the destination oin the database.

--- database/flights.model.sql
+++ database/flights.model.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS public.flights (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  destination VARCHAR(255) NOT NULL,
   departure_planet_id UUID REFERENCES planets(id),
   arrival_planet_id UUID REFERENCES planets(id),
   departure_time TIMESTAMP NOT NULL,
   return_time TIMESTAMP NOT NULL,
   price NUMERIC(10,2) NOT NULL,
   flight_number VARCHAR(20) NOT NULL UNIQUE,
-  destination VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
