8ca35342f6c7f8974c481600151ab2725768cc14
Sobrielle <sgscalalay@gmail.com>
2025-07-15 12:33:25 +0800

    fix(Explr-Uranus-Venus): Rename the file images and updated the path

--- pages/exp-planet/Uranus/Uranus.php
+++ pages/exp-planet/Uranus/Uranus.php
@@ -32,9 +32,9 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/utils/handleaccount.php';
     <div id="box-explr">
         <div class="carousel-container">
             <div class="carousel-slide">
-                <img src="assets/img/URANUS (1).png" draggable="false">
-                <img src="assets/img/URANUS (2).png" draggable="false">
-                <img src="assets/img/URANUS (3).png" draggable="false">
+                <img src="assets/img/URANUS_1.png" draggable="false">
+                <img src="assets/img/URANUS_2.png" draggable="false">
+                <img src="assets/img/URANUS_3.png" draggable="false">
             </div>
             <button class="carousel-btn prev-btn">&lt;</button>
             <button class="carousel-btn next-btn">&gt;</button>
