3389031562540e9dc326821d7fd1777526d6adf2
AltheaZuleika <sadiconaltheazuleika@gmail.com>
2025-07-15 10:42:02 +0800

    feat(booking): add favicon to the booking page

--- pages/booking/booking.php
+++ pages/booking/booking.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Passenger Details</title>
     <link rel="stylesheet" href="/pages/booking/assets/css/styles.css">
+    <link rel="icon" type="image/x-icon" href="assets/img/TED_LOGOwoBG.png">
 </head>
 <body>
     <!-- Background Elements -->
